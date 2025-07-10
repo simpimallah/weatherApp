713f56acff53a23bd8df36c7a9be4f8c379e92fc
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-10 13:57:49 +0530

    add

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,20 +1,20 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { config } from './env';
-import { User } from '../entities/User';
-import { City } from '../entities/City';
-import { WeatherHistory } from '../entities/WeatherHistory';
+// import { config } from './env';
+// import { User } from '../entities/User';
+// import { City } from '../entities/City';
+// import { WeatherHistory } from '../entities/WeatherHistory';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: config.DB_HOST,
-  port: config.DB_PORT,
-  username: config.DB_USERNAME,
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
-  synchronize: true, // Set to false in production
-  logging: false,
-  entities: [User, City, WeatherHistory],
-  migrations: [],
+ type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true, // ⚠️ disable in production
+  logging: true,
+  entities: ['dist/entities/*.js'], // adjust if needed
+  migrations: ['dist/migrations/*.js'],
   subscribers: [],
 });
