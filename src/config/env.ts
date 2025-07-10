626021533a50de32522abae563ed6f9a104d17e9
radhkrishna1102 <divinerktechnology@gmail.com>
2025-07-10 13:51:08 +0530

    change env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  PORT: process.env.PORT || 5000,
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_PORT: parseInt(process.env.DB_PORT || '5432', 10),
-  DB_USERNAME: process.env.DB_USERNAME || 'postgres',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'admin',
-  DB_NAME: process.env.DB_NAME || 'weatherdb',
-  JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
+  // PORT: process.env.PORT || 5000,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: parseInt(process.env.DB_PORT || '5432'),
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
